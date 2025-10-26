38e8438fa31fa342d71296204274f50357877691
DanielSalis <danielsalis98@gmail.com>
2025-10-26 13:07:42 +0000

    feat: create install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# install.sh
+
+set -e
+echo " 🚀 Iniciando a instalação do RD Setup..."
+
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPTS_DIR="$PROJECT_DIR/scripts"
+
+echo "⚙️  Executando instalações essenciais..."
+for script in "$SCRIPTS_DIR"/common/*.sh; do 
+    echo " -> Executando $(basename "$script")..."
+    bash "$script"  
+done
+
+
+echo "🎨 Aplicando configurações..."
+for script in "$SCRIPTS_DIR"/configs/*.sh; do 
+    echo " -> Executando $(basename "$script")..."
+    bash "$script"  
+done
+
+echo "✅ Instalação concluída com sucesso!"
+echo "Necessario fazer logout para aplicar todas as alteracoes"
